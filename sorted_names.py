a7a53119781d4479c7af39bd3ea90ecbcb8ca04a
Martin Yanchev <martinm199913@gmail.com>
2018-06-13 01:07:16 +0300

    Add comments

--- sorted_names.py
+++ sorted_names.py
@@ -1,9 +1,15 @@
+#Getting input which define how much name we will read from user
 n = int(input())
+#declarate empty list which will contain names
 names = []
+# for loop for each name
 for i in range(0,n):
     new_names = input()
+#append new name to a list of names
     names.append(new_names)
+#sort all names
 names.sort()
 print("Sorted names are:")
+#print all names
 for name in names:
     print(name)
